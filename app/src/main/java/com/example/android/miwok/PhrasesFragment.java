752e37005dd4366d2753187406d7637b2c8dff64
Rohan Mote <iamrohanmote@gmail.com>
2019-07-09 10:59:31 +0530

    First Commit

---
+++ app/src/main/java/com/example/android/miwok/PhrasesFragment.java
@@ -0,0 +1,138 @@
+
+package com.example.android.miwok;
+
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class PhrasesFragment extends Fragment {
+
+    private MediaPlayer mMediaPlayer;
+
+    private AudioManager mAudioManager;
+
+
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+
+
+
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                mMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+
+                releaseMediaPlayer();
+            }
+        }
+    };
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+    public PhrasesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word(R.string.phrase_where_are_you_going,
+                R.string.miwok_phrase_where_are_you_going, R.raw.phrase_where_are_you_going));
+        words.add(new Word(R.string.phrase_what_is_your_name,
+                R.string.miwok_phrase_what_is_your_name, R.raw.phrase_what_is_your_name));
+        words.add(new Word(R.string.phrase_my_name_is,
+                R.string.miwok_phrase_my_name_is, R.raw.phrase_my_name_is));
+        words.add(new Word(R.string.phrase_how_are_you_feeling,
+                R.string.miwok_phrase_how_are_you_feeling, R.raw.phrase_how_are_you_feeling));
+        words.add(new Word(R.string.phrase_im_feeling_good,
+                R.string.miwok_phrase_im_feeling_good, R.raw.phrase_im_feeling_good));
+        words.add(new Word(R.string.phrase_are_you_coming,
+                R.string.miwok_phrase_are_you_coming, R.raw.phrase_are_you_coming));
+        words.add(new Word(R.string.phrase_yes_im_coming,
+                R.string.miwok_phrase_yes_im_coming, R.raw.phrase_yes_im_coming));
+        words.add(new Word(R.string.phrase_im_coming,
+                R.string.miwok_phrase_im_coming, R.raw.phrase_im_coming));
+        words.add(new Word(R.string.phrase_lets_go,
+                R.string.miwok_phrase_lets_go, R.raw.phrase_lets_go));
+        words.add(new Word(R.string.phrase_come_here,
+                R.string.miwok_phrase_come_here, R.raw.phrase_come_here));
+
+
+        WordAdapter adapter = new WordAdapter(getActivity(), words, R.color.category_phrases);
+
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                releaseMediaPlayer();
+
+                Word word = words.get(position);
+
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+
+
+                    mMediaPlayer = MediaPlayer.create(getActivity(), word.getAudioResourceId());
+
+                    mMediaPlayer.start();
+
+
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+
+        releaseMediaPlayer();
+    }
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+
+            mMediaPlayer.release();
+
+
+            mMediaPlayer = null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,57 @@
+
+package com.example.android.miwok;
+
+
+public class Word {
+
+    private int mDefaultTranslationId;
+
+    private int mMiwokTranslationId;
+
+    private int mAudioResourceId;
+
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+
+    public Word(int defaultTranslationId, int miwokTranslationId, int audioResourceId) {
+        mDefaultTranslationId = defaultTranslationId;
+        mMiwokTranslationId = miwokTranslationId;
+        mAudioResourceId = audioResourceId;
+    }
+
+
+    public Word(int defaultTranslationId, int miwokTranslationId, int imageResourceId,
+                int audioResourceId) {
+        mDefaultTranslationId = defaultTranslationId;
+        mMiwokTranslationId = miwokTranslationId;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+
+    public int getDefaultTranslationId() {
+        return mDefaultTranslationId;
+    }
+
+
+    public int getMiwokTranslationId() {
+        return mMiwokTranslationId;
+    }
+
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+}
